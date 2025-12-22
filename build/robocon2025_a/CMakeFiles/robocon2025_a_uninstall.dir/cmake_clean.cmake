bde291c727de32dd92771eadbe7c74313a75d32a
git-shoyo <mc.merry57@gmail.com>
2025-12-22 15:08:49 +0900

    delete unnecessary

--- build/robocon2025_a/CMakeFiles/robocon2025_a_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/robocon2025_a_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/robocon2025_a_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
